8c62c15f3bda6c7659729384bd617108fade1263
guillermogutz <guille_gtzg@hotmail.com>
2018-11-23 14:24:07 -0600

    ejercicio js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// jQuery = non conflict mode
+// $ = conflict mode
+// inicializacion
+//jQuery(document) 
+$(document).ready(()=> {
+    //alert ("ALERTAAAAAA!! VAMOS A MORIRRR")
+    alert(mul(suma(10,10),10))
+
+//funciones estaticas
+function suma(valor1=1,valor2=1){
+   
+    //console.log(100+200)
+    return valor1+valor2
+
+}
+    function mul(valor=0,valor2=0){
+        return  valor1+valor2
+    }
+    
+    function imprime (){
+       return $('.participantes_item_text_h2').append(parametro)
+    }
+    
+    
