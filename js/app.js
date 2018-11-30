14cdea38e4564161672177934001e59b718d2d6b
guillermogutz <guille_gtzg@hotmail.com>
2018-11-30 13:57:47 -0600

    ejercicio js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,84 @@
 // jQuery = non conflict mode
 // $ = conflict mode
 // inicializacion
-//jQuery(document) 
-$(document).ready(()=> {
-    //alert ("ALERTAAAAAA!! VAMOS A MORIRRR")
-    alert(mul(suma(10,10),10))
+// jQuery(document)
+$(document).ready( () => {
 
-//funciones estaticas
-function suma(valor1=1,valor2=1){
-   
-    //console.log(100+200)
-    return valor1+valor2
+  // alert("ALERTAAAAA!! VAMOS A MORIRRR")
+
+  // alert(suma(10,10))
+  // alert(suma(10,10) + mul(10,10))
+  // alert(mul(suma(10,10),10))
+
+// imprime(mul(3,9))
+// regresa()
+// fondo('lime')
+// ancho()
+
+colores_fondo(250,["#3465fa","purple","black","yellow","lime","orange"])
+
+} )
+
+
+// funciones estaticas
+function suma(valor1=1,valor2=1) {
+  // console.log( 100 + 200)
+  return valor1 + valor2
+}
+
+function mul(valor1,valor2=10) {
+
+  return valor1*valor2
+}
+
+function imprime(parametro) {
+
+  return $('.participantes_item_text h2').html(parametro)
+  // return $('.participantes_item_text h2').append(paramet$('.participantes_item_text h4').html()
 
 }
-    function mul(valor=0,valor2=0){
-        return  valor1+valor2
+
+function regresa() {
+
+  var x = $('.participantes_item_text h4').html()
+
+  return alert(x)
+
+}
+
+function fondo(color) {
+
+  $('body').css('background-color', color)
+  // $('body').css({'':''})
+
+}
+
+function ancho() {
+  // eventos
+  $(window).on('resize', ()=>{
+
+    var x = $(document).width()
+    // console.log("RESIZE")
+    console.log(x)
+
+  })
+
+}
+
+function colores_fondo(time=1000,colorArray=['purple','red']) {
+
+  var i = 0;
+  var size = colorArray.length;
+
+  setInterval(()=> {
+    if(i == size-1) {
+      i=0
+    } else {
+      i++
     }
-    
-    function imprime (){
-       return $('.participantes_item_text_h2').append(parametro)
-    }
-    
+    $('body').css('background-color',colorArray[i])
+  },time)
+
+  // return i
+}
     
